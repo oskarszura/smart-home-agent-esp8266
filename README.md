15abe53a72f5fe92f9902f5636ba623b1e77a3f3
Oskar Szura <oskar.szura@gmail.com>
2018-07-04 18:24:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# smart-home-agent-esp8266
---
+++ main/main.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h> 
+ 
+ESP8266WebServer server(80);
+
+String readString;
+
+const String agentId = "SHA-1";
+ 
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin("hawaiinet1", "POK99MNB");
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+ 
+  server.on("/", handleRootPath);   
+  server.on("/api", handleApiPath);
+  
+  server.begin();                   
+}
+ 
+void loop() {
+  if (Serial.available()) {
+    char c = Serial.read();  
+    readString += c;
+  }
+ 
+  server.handleClient();
+}
+
+void handleRootPath() {
+  server.send(200, "text/plain", "Agent Id: " + agentId + " IP address: " + WiFi.localIP());
+}
+
+void handleApiPath() {
+  server.send(200, "text/plain", readString);
+  readString = "";
+}
+
