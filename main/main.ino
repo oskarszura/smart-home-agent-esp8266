7bede83c17af6242a5dcae1c180b5bd9b68c83b0
Oskar Szura <oskar.szura@gmail.com>
2019-02-01 19:41:28 +0100

    HOME-104 - Make config path to change WIFI ssid and password

--- main/main.ino
+++ main/main.ino
@@ -1,24 +1,32 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h> 
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
  
 ESP8266WebServer server(80);
+const char* ssid = "ESPWebServer";
+const char* password = "12345678";
 
 String readString;
-
-const String agentType = "SHA-0.2.0";
  
 void setup() {
   Serial.begin(9600);
-  WiFi.begin("", "");
+  //WiFi.begin("", "");
  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+  //while (WiFi.status() != WL_CONNECTED) {
+  //  delay(500);
+  //}
+  WiFi.mode(WIFI_AP);           //Only Access point
+  WiFi.softAP(ssid, password);  //Start HOTspot removing password will disable security
+ 
+  IPAddress myIP = WiFi.softAPIP();
  
   server.on("/", handleRootPath);   
   server.on("/api", handleApiPath);
+  server.on("/config", handleConfigPath);
   
-  server.begin();                   
+  server.begin();        
+
+  EEPROM.begin(512);
 }
  
 void loop() {
@@ -31,7 +39,7 @@ void loop() {
 }
 
 void handleRootPath() {
-  server.send(200, "text/plain", "Agent type: " + agentType  + " IP address: " + WiFi.localIP());
+  server.send(200, "text/plain", "Hardware: agent-type-1");
 }
 
 void handleApiPath() {
@@ -39,3 +47,41 @@ void handleApiPath() {
   readString = "";
 }
 
+void handleConfigPath() {
+  const String ssid = server.arg("ssid");
+  const String pass = server.arg("pass");
+  const int maxLen = 20;
+
+  if (ssid != "" && pass != "") {
+    for(int n = 0; n < maxLen; n++) {
+      char ssidChar = ' ';
+      char passChar = ' ';
+  
+      if (n < ssid.length()) {
+           ssidChar = ssid[n];
+      }
+      if (n < pass.length()) {
+           passChar = pass[n];
+      }    
+  
+      EEPROM.write(n, ssidChar); 
+      EEPROM.write(maxLen + n, passChar); 
+    }
+    
+    EEPROM.commit();
+    server.send(200, "text/plain", "Persisted");
+  } else {
+    String ssid = "";
+    String pass = "";
+    
+    for(int n = 0; n < maxLen; n++) {
+      ssid += char(EEPROM.read(n)); 
+      pass += char(EEPROM.read(maxLen + n)); 
+    }
+
+    ssid.trim();
+    pass.trim();
+    
+    server.send(200, "text/plain", "[" + ssid + "|" + pass + "]");
+  }
+}
